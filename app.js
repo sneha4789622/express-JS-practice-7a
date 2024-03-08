ce41fd80283b40d9cf44193e4d69c978f2212352
Sneha Bharti <155159002+sneha4789622@users.noreply.github.com>
2024-03-09 02:37:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -138,9 +138,8 @@ app.get(`/matches/:matchId/players`, async (request, response) => {
   const {matchId} = request.params
   const getMatchPlayersQuery = `
 	    SELECT
-	      player_details.player_id AS playerId,
-	      player_details.player_name AS playerName
-	    FROM player_match_score NATURAL JOIN player_details
+          *
+        FROM player_match_score NATURAL JOIN player_details
         WHERE match_id=${matchId};`
   const MatchPlayers = await database.all(getMatchPlayersQuery)
   response.send(
@@ -152,26 +151,20 @@ app.get(`/matches/:matchId/players`, async (request, response) => {
 
 // API 7
 
-app.get(`/players/:playerId/playerScores`, async (request, response) => {
+app.get('/players/:playerId/playerScores/', async (request, response) => {
   const {playerId} = request.params
-  const getPlayerScoredQuery = `
+  const getmatchPlayersQuery = `
     SELECT
-    player_details.player_id AS playerId,
-    player_details.player_name AS playerName,
-    SUM(player_match_score.score) AS totalScore,
-    SUM(fours) AS totalFours,
-    SUM(sixes) AS totalSixes FROM 
-    player_details INNER JOIN player_match_score ON
-    player_details.player_id = player_match_score.player_id
-    WHERE player_details.player_id = ${playerId};
-    `
-  const PlayerScored = await database.all(getPlayerScoredQuery)
-  response.send({
-    playerId: score[' player_details.player_id'],
-    playerName: score['player_details.player_name'],
-    totalScores: score['SUM(player_match_score.score)'],
-    totatFours: score['SUM(fours)'],
-    totatSixes: score['SUM(sixes)'],
-  })
+      player_id AS playerId,
+      player_name AS playerName,
+      SUM(score) AS totalScore,
+      SUM(fours) AS totalFours,
+      SUM(sixes) AS totalSixes
+    FROM player_match_score
+      NATURAL JOIN player_details
+    WHERE
+      player_id = ${playerId};`
+  const playersMatchDetails = await database.get(getmatchPlayersQuery)
+  response.send(playersMatchDetails)
 })
 module.exports = app
